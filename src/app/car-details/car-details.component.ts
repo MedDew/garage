99aec68e2f5daedfe1d8c1f90a962d2a7fcdc506
Med <mehdi.gacem@versusmind.eu>
2019-09-15 20:22:02 +0200

    implement all the methods to get car by its id

--- src/app/car-details/car-details.component.ts
+++ src/app/car-details/car-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CarService } from '../car.service';
+import { Car } from '../shared/car.model';
 
 @Component({
   selector: 'app-car-details',
@@ -8,14 +9,29 @@ import { CarService } from '../car.service';
   styleUrls: ['./car-details.component.css']
 })
 export class CarDetailsComponent implements OnInit {
+  private car : Car;
 
   constructor(private carService : CarService, private activatedRoute : ActivatedRoute) { }
 
   ngOnInit() {
+    this.loadCarById();
   }
 
-  private getCarById(){
-    const carId = this.activatedRoute.snapshot.paramMap.get("carId");
-    
+  private loadCarById(){
+    const carId = this.activatedRoute.snapshot.paramMap.get("id");
+    console.log("carId : "+carId);
+    this.carService.getCar(carId).subscribe(car => { 
+      console.log("CAR BY ID", car);
+      this.setCar(car);
+    });
   }
+
+  private setCar(car : Car): void{
+    this.car = car;
+  }
+  
+  public getCar(): Car{
+    return this.car;
+  }
+
 }
