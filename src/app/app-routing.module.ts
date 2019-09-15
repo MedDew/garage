1478d35fd0c2682bfd87a06902d621ec4b39c9f1
Med <mehdi.gacem@versusmind.eu>
2019-09-15 19:48:12 +0200

    declare the route to get a car by its id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CarListComponent } from './car-list/car-list.component';
 import { CarCreateComponent } from './car-create/car-create.component';
+import { CarDetailsComponent } from './car-details/car-details.component';
 
 const routes: Routes = [
   {path : "cars", component : CarListComponent },
   {path : "car", component : CarCreateComponent },
+  {path : "car/:id", component : CarDetailsComponent },
 ];
 
 @NgModule({
