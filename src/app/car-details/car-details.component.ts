7ca5c59024e765a4ae85072a740cb54eadc2aed4
Med <mehdi.gacem@versusmind.eu>
2019-09-15 19:49:54 +0200

    create a method to recover the car to find + injecting activatedroute to get the route param + injetct the car service to make the request and get the car

--- src/app/car-details/car-details.component.ts
+++ src/app/car-details/car-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CarService } from '../car.service';
 
 @Component({
   selector: 'app-car-details',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private carService : CarService, private activatedRoute : ActivatedRoute) { }
 
   ngOnInit() {
   }
 
+  private getCarById(){
+    const carId = this.activatedRoute.snapshot.paramMap.get("carId");
+    
+  }
 }
