f5fe9ce0fd054442e9dc7e959769c2d9a310f496
Med <mehdi.gacem@versusmind.eu>
2019-09-15 14:47:51 +0200

    add routing for car creation form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CarListComponent } from './car-list/car-list.component';
+import { CarCreateComponent } from './car-create/car-create.component';
 
 const routes: Routes = [
-  {path : "cars", component : CarListComponent }
+  {path : "cars", component : CarListComponent },
+  {path : "car", component : CarCreateComponent },
 ];
 
 @NgModule({
