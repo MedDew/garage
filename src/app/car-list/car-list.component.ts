755e26eba96a46faa8790964017c922213879f1a
Med <mehdi.gacem@versusmind.eu>
2019-09-15 14:20:02 +0200

    use carService to get the list of cars

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CarService } from '../car.service';
+import { Car } from '../shared/car.model';
 
 @Component({
   selector: 'app-car-list',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarListComponent implements OnInit {
 
-  constructor() { }
+  private carList : Car[];
+
+  constructor(private carService : CarService) { }
 
   ngOnInit() {
+    this.initCarList();  
   }
 
+  private initCarList() : void{
+    this.carService.getCars().subscribe(result => {
+      console.log("RESULT",result);
+      this.setCarList(result);
+    });
+  }
+
+  private setCarList(carList : Car[]){
+    this.carList = carList;
+  }
+  
+  public getCarList() {
+    return this.carList;
+  }
+  
 }
