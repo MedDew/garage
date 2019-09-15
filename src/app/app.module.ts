cbbfb37d42f437a3845bae02cf2d10108ab23861
Med <mehdi.gacem@versusmind.eu>
2019-09-15 14:19:02 +0200

    add HttpClientModule to make http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarListComponent } from './car-list/car-list.component';
+import { APPCONFIG } from './APPCONFIG';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { CarListComponent } from './car-list/car-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{provide : APPCONFIG , useValue : APPCONFIG.CARS_API_URL}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
